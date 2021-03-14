c9edb16d463286a41f84575cbcf1539f8dc2f099
Vladislavsavi <vladislavsavi@gmail.com>
2021-03-14 18:26:37 +0600

    feature iE9 add browserslistrc replaced dataset with data

--- src/utils/BaseComponent/index.ts
+++ src/utils/BaseComponent/index.ts
@@ -1,16 +1,23 @@
+interface TargetElement extends HTMLBaseElement{
+    data: any;
+}
+
 export abstract class BaseComponent {
     container: any;
     ref: number;
     refs: {[key: string]: BaseComponent} = {};
 
-    constructor(container: HTMLBaseElement) {
-        if (typeof container.dataset.ref === 'undefined') {
+    constructor(container: TargetElement) {
+        if(!container.data) {
+            container.data = {};
+        }
+        if (typeof container.data.ref === 'undefined') {
             this.ref = Math.random();
             this.refs[this.ref] = this;
-            container.dataset.ref = String(this.ref);
+            container.data.ref = String(this.ref);
             this.init(container);
         } else {
-            return this.refs[container.dataset.ref];
+            return this.refs[container.data.ref];
         }
     }
 
